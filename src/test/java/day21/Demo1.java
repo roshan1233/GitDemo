7174f9db1e9f400829ea5c44b303923fe4d4cd9c
Awanish Anand <anandawanish7@gmail.com>
2025-08-15 14:35:14 +0530

    first commit to branch

--- src/test/java/day21/Demo1.java
+++ src/test/java/day21/Demo1.java
@@ -16,5 +16,7 @@ public class Demo1 {
 		System.out.println("Demo2");
 		System.out.println("Demo3");
 		System.out.println("Demo4");
+		System.out.println("Demo5");
+		System.out.println("Demo6");
 	}
 }
